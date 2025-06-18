950e5b1b15643942084c9fd47197a42a27ad00dc
MPdoor1 <107142603+MPdoor1@users.noreply.github.com>
2025-06-18 11:52:02 -0400

    Fix piano keyboard: add all black notes and correct frequencies

--- script.js
+++ script.js
@@ -190,12 +190,16 @@ function playNote(note) {
     oscillator.connect(gainNode);
     gainNode.connect(audioContext.destination);
 
-    // Set frequency based on note
+    // Set frequency based on note - Complete piano range C4 to B5
     const frequencies = {
+        // Octave 4 (C4 to B4)
         'C4': 261.63, 'C#4': 277.18, 'D4': 293.66, 'D#4': 311.13,
         'E4': 329.63, 'F4': 349.23, 'F#4': 369.99, 'G4': 392.00,
         'G#4': 415.30, 'A4': 440.00, 'A#4': 466.16, 'B4': 493.88,
-        'C5': 523.25
+        // Octave 5 (C5 to B5)
+        'C5': 523.25, 'C#5': 554.37, 'D5': 587.33, 'D#5': 622.25,
+        'E5': 659.25, 'F5': 698.46, 'F#5': 739.99, 'G5': 783.99,
+        'G#5': 830.61, 'A5': 880.00, 'A#5': 932.33, 'B5': 987.77
     };
 
     oscillator.frequency.setValueAtTime(frequencies[note] || 440, audioContext.currentTime);
